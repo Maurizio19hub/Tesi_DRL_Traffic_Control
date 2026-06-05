accface8eda357f98ce2a6234a61aea0fe3bc3bc
Maurizio19hub <mrzcamposeo@gmail.com>
2026-06-05 11:35:35 +0200

    Aggiunta tempo attesa pedoni alla reward con energia, senza reward collisioni
    Rimane da verificare se si riesce a integrare una buona reward per collisioni

--- results/res.py
+++ results/res.py
@@ -4,7 +4,7 @@ from stable_baselines3.common.monitor import load_results
 from stable_baselines3.common.results_plotter import rolling_window
 
 # Carica i risultati
-df = load_results("../models/correct_time/t20energy-collisions_f2")
+df = load_results("../models/correct_time/t20energy-pedwaiting_f2")
 
 # Grafico con media mobile a 10 episodi
 plt.figure(figsize=(12, 5))
@@ -15,7 +15,7 @@ plt.ylabel('Reward')
 plt.title('Training reward')
 plt.legend()
 plt.grid(True)
-plt.savefig("../models/correct_time/t20energy-collisions_f2/training_curve.png", dpi=150)
+plt.savefig("../models/correct_time/t20energy-pedwaiting_f2/training_curve.png", dpi=150)
 plt.show()
 
 print(df.describe())
